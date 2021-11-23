24602045295352edd1cf2b364d4142638262a571
alexa <alexa@LAPTOP-F3VUP7R2>
2021-11-22 22:05:43 -0500

    Se añadio cambio de panel al pulsar crearCuenta

--- src/main/java/Controlador/Interactividad.java
+++ src/main/java/Controlador/Interactividad.java
@@ -4,10 +4,59 @@
  */
 package Controlador;
 
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import vista.ForgottenPassword;
+import vista.LoginFrame;
+import vista.SingUp;
+
 /**
  *
  * @author alexa
  */
 public class Interactividad {
+    //vista
+    private LoginFrame frame = new LoginFrame();
+    private SingUp crearCuenta = new SingUp();
+    private ForgottenPassword recuperarContrasenia = new ForgottenPassword();
+    //
+    private Point LocationPanels = frame.getLoginPanelLocation();
+    //
+    public Interactividad(){
+        crearCuenta.setBounds((int)LocationPanels.getX(),(int)LocationPanels.getY(), 340, 465);
+        recuperarContrasenia.setBounds((int)LocationPanels.getX(),(int)LocationPanels.getY(), 340, 465);
+        this.loginFrame_Interaction();
+        
+    }
+    
+    private void loginFrame_Interaction(){
+        
+        frame.setVisible(true);
+        frame.getBt_crear().addActionListener(new ActionListener(){
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               frame.getLoginPanel().setVisible(false);
+               crearCuenta.setVisible(true);
+               frame.getCapaBase().add(crearCuenta);
+               frame.revalidate();
+            }    
+        });
+        frame.getBt_ingresar().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Posteriormente se implementara el frame de ingreso");
+            }    
+        });
+        frame.getBt_recuperar().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+            
+        });
+    }
     
 }
