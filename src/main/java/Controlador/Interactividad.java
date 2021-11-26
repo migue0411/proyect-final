1093ee4b4558d5d652bd2fb9a11c20e5fc825775
alexa <alexa@LAPTOP-F3VUP7R2>
2021-11-25 20:22:34 -0500

    se ha añadido eventos para el funcionamiento del panel forgottenPassword

--- src/main/java/Controlador/Interactividad.java
+++ src/main/java/Controlador/Interactividad.java
@@ -4,14 +4,20 @@
  */
 package Controlador;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.MessagingException;
 import javax.swing.JButton;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
 import modelo.Login;
 import modelo.Recover;
 import vista.ForgottenPassword;
@@ -27,7 +33,7 @@ public class Interactividad {
     //vista
     private LoginFrame frame = new LoginFrame();
     private SingUp crearCuenta = new SingUp();
-    private ForgottenPassword recuperarContrasenia = new ForgottenPassword();
+    private ForgottenPassword recoveryView = new ForgottenPassword();
     private Home homeFrame = new Home();
     private Login login = new Login();
     private Recover recuperar = new Recover();
@@ -37,7 +43,7 @@ public class Interactividad {
     //
     public Interactividad(){
         crearCuenta.setBounds((int)LocationPanels.getX(),(int)LocationPanels.getY(), 343, 469);
-        recuperarContrasenia.setBounds((int)LocationPanels.getX(),(int)LocationPanels.getY(), 343, 469);
+        recoveryView .setBounds((int)LocationPanels.getX(),(int)LocationPanels.getY(), 343, 469);
         this.loginFrame_Interaction();
         this.recoveryMethod(); 
     }
@@ -68,37 +74,109 @@ public class Interactividad {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.getLoginPanel().setVisible(false);//panel de login 
-                recuperarContrasenia.setVisible(true);//vista
-                frame.getCapaBase().add(recuperarContrasenia);//se le añade a la capa base otro panel 
+                recoveryView.setVisible(true);//vista
+                frame.getCapaBase().add(recoveryView);//se le añade a la capa base otro panel 
                 frame.revalidate();
             }
             
         });
     }
     private void recoveryMethod(){
-        recuperarContrasenia.getBt_Enviar().addActionListener(new ActionListener(){
+        Color redError = new Color(136,145,145);
+        recoveryView.getBt_Enviar().addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    if(!recuperar.sendRecoverCode(recuperarContrasenia.getCampo_Correo().getText())){
+                    if(!recuperar.sendRecoverCode(recoveryView.getCampo_Correo().getText())){
                         System.out.println("No se pudo enviar el codigo de recuperacion, ingrese un correo valido");
                     }
+                    recoveryView.getCampo_Codigo().setEnabled(true);
+                    recoveryView.getBt_verficarCodigo().setEnabled(true);
                 } catch (MessagingException ex) {
                     Logger.getLogger(Interactividad.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
             
         });
-        recuperarContrasenia.getBt_verficarCodigo().addActionListener(new ActionListener(){
+        recoveryView .getBt_verficarCodigo().addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(recuperarContrasenia.getCampo_Codigo().equals(recuperar.getRecoveryCode())){
-                    recuperarContrasenia.getCampo_nuevaContrasenia1().setEditable(true);
-                    recuperarContrasenia.getCampo_nuevaContrasenia2().setEditable(true);
+                System.out.println(recoveryView.getCampo_Codigo().getText() + " " + recuperar.getRecoveryCode());
+                if(recoveryView.getCampo_Codigo().getText().equals(recuperar.getRecoveryCode())){
+                    recoveryView.getCampo_nuevaContrasenia1().setEnabled(true);
+                    recoveryView.getCampo_nuevaContrasenia2().setEnabled(true);
+                    recoveryView.getBt_Confirmar().setEnabled(true);
                 }
             }
             
         });
+        recoveryView.getBt_Confirmar().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }        
+        });
+        recoveryView.getBt_Volver().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                recoveryView.setVisible(false);//vista
+                frame.getLoginPanel().setVisible(true);//panel de login 
+                frame.revalidate();
+            }    
+        });
         
+        recoveryView.getBt_Confirmar().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(String.valueOf(recoveryView.getCampo_nuevaContrasenia1().getPassword()).equals(String.valueOf(recoveryView.getCampo_nuevaContrasenia2().getPassword()))){
+                    
+                    //se debe implementar un cambio de contraseña en la base de datos
+                }else{
+                    recoveryView.getCampo_nuevaContrasenia1().setBackground(redError);
+                    recoveryView.getCampo_nuevaContrasenia2().setBackground(redError);
+                }
+            }
+            
+        });
+        Document documentoPassword1 = recoveryView.getCampo_nuevaContrasenia1().getDocument();
+        Document documentoPassword2 = recoveryView.getCampo_nuevaContrasenia2().getDocument();
+        documentoPassword1.addDocumentListener(new DocumentListener(){
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                if(recoveryView.getCampo_nuevaContrasenia1().getBackground().equals(redError)){
+                    recoveryView.getCampo_nuevaContrasenia1().setBackground(Color.WHITE);
+                }
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                
+            }
+            
+        });
+        documentoPassword2.addDocumentListener(new DocumentListener(){
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                if(recoveryView.getCampo_nuevaContrasenia2().getBackground().equals(redError)){
+                    recoveryView.getCampo_nuevaContrasenia2().setBackground(Color.WHITE);
+                }    
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                
+            }
+            
+        });
     }
 }
